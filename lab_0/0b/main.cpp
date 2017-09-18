db5b68cc08298a0dbab99c927b967b521408f6f4
Anastasiya Shadrina <a.shadrina5@mail.ru>
2017-09-19 01:14:14 +0700

    0b done

---
+++ lab_0/0b/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include "module1.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        cout << "no parameters";
+        return 0;
+    }
+
+    ifstream fin(argv[1]);
+    ofstream fout(argv[2]);
+    list<string> stringsList;
+
+    string s;
+    while (!fin.eof()) {
+        getline(fin, s);
+        stringsList.push_back(s);
+    }
+
+    Module1::sort_strings(stringsList);
+
+    while (!stringsList.empty()) {
+        fout << stringsList.front() << "\n";
+        stringsList.pop_front();
+    }
+
+    fin.close();
+    fout.close();
+    return 0;
+}
---
+++ lab_0/0b/module1.h
@@ -0,0 +1,7 @@
+#include <list>
+#include <string>
+
+namespace Module1 {
+    void sort_strings(std::list<std::string> &stringsList);
+}
+
